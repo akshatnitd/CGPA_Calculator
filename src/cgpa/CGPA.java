71b993b0efc32412362a7033bf875abc90020bd6
Akshat Garg <coolakshat24@gmail.com>
2016-07-17 22:35:40 +0530

    First Commit

---
+++ src/cgpa/CGPA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cgpa;
+
+/**
+ *
+ * @author USER
+ */
+public class CGPA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        welcome a=new welcome();
+        a.setVisible(true);
+    }
+    
+}
